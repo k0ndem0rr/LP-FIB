df7bb565be05d3fafdadba238ca4d71f28f2aa57
jose.angel.conde <jose.angel.conde@estudiantat.upc.edu>
2024-02-28 23:44:03 +0100

    .

---
+++ Lab 2/P31745.hs
@@ -0,0 +1,21 @@
+flatten :: [[Int]] -> [Int]
+flatten x = foldl (++) [] x
+
+makeOne:: Char -> Int
+makeOne x = 1
+
+myLength :: String -> Int
+myLength x = foldl (+) 0 (map (makeOne) x) 
+
+myReverse :: [Int] -> [Int]
+myReverse x = foldr (f) [] x
+    where 
+        f a b = b ++ (take 1 (repeat a)) 
+
+countIn :: [[Int]] -> Int -> [Int]
+countIn list n = map (count) list
+    where 
+        count = length . (filter ((==) n))
+
+firstWord :: String -> String
+firstWord s = takeWhile ((/=) ' ') (dropWhile ((==) ' ') s)
